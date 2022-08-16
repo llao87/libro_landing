0a2d1060d1e967d784e08d19099a9346154f9b55
Dmitry LLao87 Kragel <dmitry.kragel@yandex.ru>
2022-08-16 17:51:13 +0300

    complete desktop version + fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,36 @@
-jQuery(function() {
+jQuery(function () {
+    // появление кнопки "наверх" при прокрутке
+    jQuery(window).on("scroll", function (e) {
+        if (jQuery(this).scrollTop() > 700) {
+            jQuery(".js-to-top").stop().fadeIn(300);
+        } else {
+            jQuery(".js-to-top").stop().fadeOut(300);
+        }
+    });
 
-    jQuery('.variant').on('click', function(e) {
-            e.preventDefault();
-            let currValue = jQuery(this).find('.variant-text').text();
+    // прокрутка к верху страницы
+    jQuery(".js-to-top").on("click", function (e) {
+        e.preventDefault();
+        jQuery("body,html").animate(
+            {
+                scrollTop: 0,
+            },
+            800
+        );
+        return false;
+    });
 
-            jQuery('.variant').removeClass('active');
-            jQuery(this).addClass('active');
-            jQuery('.js-investment-amount').text(currValue);
-        })
-        // $('.single-slider').jRange({
-        //     from: -2.0,
-        //     to: 2.0,
-        //     step: 0.5,
-        //     scale: ["до 500 тыс. руб."-2.0,-1.0,0.0,1.0,2.0],
-        //     // format: '%s',
-        //     width: 1000,
-        //     showLabels: true,
-        //     snap: true
-        // });
+    // плавный скролл от меню до блока
+    jQuery(".nav-link").on("click", function (e) {
+        e.preventDefault();
+        let clickTarget = jQuery(this).attr("href");
+
+        jQuery("body,html").animate(
+            {
+                scrollTop: jQuery(clickTarget).offset().top
+            },
+            800
+        );
+        return false;
+    });
 });
